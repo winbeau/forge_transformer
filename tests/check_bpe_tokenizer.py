cf4c6581d95fe6f6df3549f84f334c3002d53044
winbeau <geneva4869@163.com>
2025-12-21 20:48:49 +0800

    feat: add logit for checking encode <|endoftext|>

--- tests/check_bpe_tokenizer.py
+++ tests/check_bpe_tokenizer.py
@@ -8,7 +8,6 @@ Usage:
 from forge_transformer.bpe import Tokenizer
 from forge_transformer import BASE_DIR
 
-
 tok = Tokenizer.from_files(
     BASE_DIR / "bpe_model/vocab.json",
     BASE_DIR / "bpe_model/merges.txt",
@@ -16,21 +15,34 @@ tok = Tokenizer.from_files(
 )
 
 
-begin = " Test: bpe tokenizer "
-end = " Test End "
-d = (len(begin) - len(end)) // 2
-print("=" * 30 + begin + "=" * 30)
-print("请输入单词（ctrl+d 退出）:")
+def check_eot():
+    print("Test <|endoftext|>:")
+    print(tok.encode("<|endoftext|>"))
 
-while True:
-    try:
-        line = input()
-        for s in line.split():
-            ids = tok.encode(s)
-            print("ids:", ids)
-            print("len:", len(ids))
-            print("decoded:", tok.decode(ids))
 
-    except EOFError:
-        print("=" * (30 + d) + end + "=" * (30 + d))
-        break
+def main():
+    begin = " Test: bpe tokenizer "
+    end = " Test End "
+    d = (len(begin) - len(end)) // 2
+    print("=" * 30 + begin + "=" * 30)
+
+    check_eot()
+
+    print("请输入单词（ctrl+d 退出）:")
+
+    while True:
+        try:
+            line = input()
+            for s in line.split():
+                ids = tok.encode(s)
+                print("ids:", ids)
+                print("len:", len(ids))
+                print("decoded:", tok.decode(ids))
+
+        except EOFError:
+            print("=" * (30 + d) + end + "=" * (30 + d))
+            break
+
+
+if __name__ == "__main__":
+    main()
